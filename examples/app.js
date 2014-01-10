578b25342cdaba735d63fa640e8de50d35fd2535
Brooks Boyd <boydb@midnightdesign.ws>
2014-01-10 16:43:34 -0600

    Update example

--- examples/app.js
+++ examples/app.js
@@ -1,4 +1,5 @@
 var BTCNetwork = require('../lib/BTCNetwork').BTCNetwork;
+var dns = require('dns');
 
 var n = new BTCNetwork();
 
@@ -20,12 +21,10 @@ n.on('peerStatus', function status(d) {
   console.log('PeerManager status:', d);
 });
 
-/*
 // Every message, from every active peer. Gets the message in its raw form (bubble up of 'message' from Peer)
 n.on('message', function peerMessage(d) {
-  console.log(d.peer.getUUID()+': message', d.command, d.data.toString('hex'));
+  console.log(d.peer.getUUID()+': message', d.command, d.data);
 });
-*/
 
 // Specific message from the BTCManager. Gets the message in its parsed form
 n.on('versionMessage', function versionMessage(d) {
@@ -39,17 +38,19 @@ n.on('transactionBlock', function transactionInv(d) {
   console.log('Peer '+d.peer.getUUID()+' knows of Block '+d.hash.toString('hex'));
 });
 
+/*
 // Default launch, with DNS seeds
 n.launch();
+*/
 
-/*
 // Single launch
+n.options.minPeers = 1;
+n.options.maxPeers = 1;
 dns.resolve4('dnsseed.bluematt.me', function(err, addrs) {
   if (err) {
     console.log(err);
     return;
   }
   n.launch(addrs.shift());
-  //n.manager.addPool(addrs);
+  n.manager.addPool(addrs);
 });
-*/
